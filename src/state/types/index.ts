1751e05ffe0b7448b8346efca52487be73500ee8
Richard Matthews <richard.matthews@wonderbill.com>
2021-02-07 14:52:53 +0000

    Add types file

---
+++ src/state/types/index.ts
@@ -0,0 +1,45 @@
+type LeagueAdmin = {
+    id: string
+}
+
+type Opponent = {
+    goals: number
+    id: string
+    name: string
+}
+
+type PlayerRound = {
+    choice: {
+        goals: number
+        hasMadeChoice: boolean
+        id: number
+        opponent: Opponent
+        result: string
+        teamPlayingAtHome: boolean
+        value: string
+    }
+}
+
+type GamePlayer = {
+    hasBeenEliminated: boolean
+    id: string
+    name: string
+    rounds: PlayerRound[]
+}
+
+type LeagueGame = {
+    id: string
+    complete: boolean
+    currentGameRound: number
+    players: GamePlayer[]
+}
+
+export type League = {
+    currentRound: number
+    id: string
+    isPrivate: boolean
+    joinPin: string
+    name: string
+    admin: LeagueAdmin
+    games: LeagueGame[]
+}
