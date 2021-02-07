024b7d33de6e69c61a6af8b12b2cb031954e0958
Chris Featley <f.chris@live.co.uk>
2021-02-07 22:49:55 +0000

    Removed log and inline style

--- src/ui-components/containers/screenComponent.tsx
+++ src/ui-components/containers/screenComponent.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
+import { StyleSheet } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { StatusBar } from 'react-native'
 
 export const ScreenComponent = ({ children, theme }: any) => {
-    console.log(theme.dark)
     return (
-        <SafeAreaView style={{ flex: 1, backgroundColor: theme.background.primary }}>
+        <SafeAreaView style={styles(theme).container}>
             <StatusBar barStyle={theme.dark ? 'light-content' : 'dark-content'} />
             {children}
         </SafeAreaView>
     )
 }
+
+const styles = (theme: any) =>
+    StyleSheet.create({
+        container: { flex: 1, backgroundColor: theme.background.primary },
+    })
